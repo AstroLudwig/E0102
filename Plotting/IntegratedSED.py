98f2edafa32f591a3676381f7f813ff590cf1a93
Bethany <ludwig@astro.utoronto.ca>
2019-02-11 15:52:40 -0500

    Remove 2 parameter confidence plot for Integrated SED

--- Plotting/IntegratedSED.py
+++ Plotting/IntegratedSED.py
@@ -23,12 +23,7 @@ from scipy.stats import iqr
 ##############
 
 save = True
-# ~~~ Plotting ~~~ #
-# Plot Integrated SED
-plot_integrated = True
-# Plot Chi Squared Confidence Intervals for Integrated SED 
-plot_integrated_ChiSquaredConfidence = True
-
+show = False
 
 ###########
 ## Files ##
@@ -113,68 +108,29 @@ total_sed = cold_sed + warm_sed
 ##########
 # Plot   #
 ##########
-if plot_integrated:
-	fig = plt.figure(figsize=(11,8))
-	plot = fig.add_subplot(111)
-	# Plot SEDs
-	plot.plot(lam,total_sed,color="#424186")
-	plot.plot(lam,warm_sed,color="#84D44B",ls='dashed') 
-	plot.plot(lam,cold_sed,color="#23A883")
-	# Plot Measured Values
-	plot.errorbar([24,70,100,160],AverageIntensities,yerr=AverageError,marker='o',linestyle='none',color="black")
-	# Plot Labels
-	plot.set_xlabel("Wavelength ($\mu m$)",size=18)
-	plot.set_ylabel("Spectral Intensity (MJy sr$^{-1}$)",size=18)
-	plot.set_title("Integrated Spectral Energy Distribution",size=20)
-	plot.legend(("Total SED","Warm SED","Cold SED"),prop={'size':14})
-	
-	plot.tick_params(axis='both', which='major', labelsize=16)
-	plot.tick_params(axis='both', which='minor', labelsize=14)
-	
-	plot.grid(color='white',linestyle='-')
-	plot.set_facecolor("#EAEAF2")
+fig = plt.figure(figsize=(11,8))
+plot = fig.add_subplot(111)
+# Plot SEDs
+plot.plot(lam,total_sed,color="#424186")
+plot.plot(lam,warm_sed,color="#84D44B",ls='dashed') 
+plot.plot(lam,cold_sed,color="#23A883")
+# Plot Measured Values
+plot.errorbar([24,70,100,160],AverageIntensities,yerr=AverageError,marker='o',linestyle='none',color="black")
+# Plot Labels
+plot.set_xlabel("Wavelength ($\mu m$)",size=18)
+plot.set_ylabel("Spectral Intensity (MJy sr$^{-1}$)",size=18)
+plot.set_title("Integrated Spectral Energy Distribution",size=20)
+plot.legend(("Total SED","Warm SED","Cold SED"),prop={'size':14})
 
-	print(("Temp {} Cold Mass {} Warm Mass {} Total Mass {} Chi Squared {} ").format(temp,cold_mass,warm_mass,cold_mass+warm_mass,chi_squared))
-	if save:
-		plt.savefig("Plots/IntegratedSED.png")
+plot.tick_params(axis='both', which='major', labelsize=16)
+plot.tick_params(axis='both', which='minor', labelsize=14)
 
-if plot_integrated_ChiSquaredConfidence:
+plot.grid(color='white',linestyle='-')
+plot.set_facecolor("#EAEAF2")
 
-	def ChiSquaredMap(Map,interval):
-		Map = np.copy(Map)
-		# Change everything outside the interval to NaN.
-		R,C = np.where(Map > (chi_squared + interval))
-		Map[R,C] = np.nan 
-		return np.copy(Map)
-
-	f, axes = plt.subplots(3,2)
+print(("Temp {} Cold Mass {} Warm Mass {} Total Mass {} Chi Squared {} ").format(temp,cold_mass,warm_mass,cold_mass+warm_mass,chi_squared))
 	
-	# What the elements actually represent on an axis
-	physical_X = [np.where(np.isclose(ColdMass,cold_mass))[0][0],
-					np.where(np.isclose(ColdTemp,temp))[0][0],
-						np.where(np.isclose(ColdMass,cold_mass))[0][0]]
-
-	physical_Y = [np.where(np.isclose(WarmMass,warm_mass))[0][0],
-					np.where(np.isclose(WarmMass,warm_mass))[0][0],
-						np.where(np.isclose(ColdTemp,temp))[0][0]]
-	
-	titles = ["68.3% Confidence","95.4% Confidence"]
-	ylabels = ["Warm Dust Mass M$_\odot$","Warm Dust Mass M$_\odot$","Temperature K",]
-	xlabels = ["Cold Dust Mass M$_\odot$", "Temperature K", "Cold Dust Mass M$_\odot$"]
-	
-	# Temperature Slice, Cold Mass Slice, Warm Mass Slice at solutions
-	chi_squared_slices = [np.copy(chi_squared_cube)[:,physical_X[1],:],
-							np.copy(chi_squared_cube)[:,:,physical_X[0]],
-								np.copy(chi_squared_cube)[physical_Y[0],:,:]]
-	
-	for i in range(3):
-		for j in range(2):
-			img = ChiSquaredMap(chi_squared_slices[i],intervals[j])
-			axes[i,j].imshow(img)
-			axes[i,j].scatter(physical_X[i],physical_Y[i],s=5,c='r')
-			axes[i,j].set_ylim(physical_Y[i]-10,physical_Y[i]+10); axes[i,j].set_xlim(physical_X[i]-10,physical_X[i]+10);
-			axes[i,j].set_xlabel(xlabels[i]); axes[i,j].set_ylabel(ylabels[i])
-   
-	axes[0,0].set_title(titles[0])
-	axes[0,1].set_title(titles[1])  
-plt.show()
+if save:
+	plt.savefig("Plots/IntegratedSED.png")
+if show:
+	plt.show()
